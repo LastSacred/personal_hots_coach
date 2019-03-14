e8df941730176c71a28462b28b57ffb2b4e14a8e
Arthur Torres <arthurtorres002@gmail.com>
2019-03-14 16:32:29 -0400

    hero search

--- src/components/HeroesSearch.js
+++ src/components/HeroesSearch.js
@@ -1,10 +1,36 @@
 import React from 'react'
-import { Container } from 'semantic-ui-react'
+import { connect } from 'react-redux'
+import { Container, Form } from 'semantic-ui-react'
+import { getHeroes } from '../services/backend'
+
+const filterHeroes = (event, dispatch) => {
+  const value = event.target.value
+
+  getHeroes().then(heroes => {
+    heroes = heroes.filter(hero => {
+      const name = hero.name.toLowerCase().replace("ú", "u").replace(/[^A-Z0-9]/ig, "")
+      const searchTerm = value.toLowerCase().replace(/[^A-Z0-9]/ig, "")
+
+      return name.includes(searchTerm)
+    })
+
+    dispatch({
+      type: 'UPDATE_HEROES',
+      heroes: heroes
+    })
+  })
+}
 
 const HeroesSearch = (props) => {
   return(
-    <h1>HeroesSearch</h1>
+    <Container>
+      <Form>
+        <Form.Field onChange={(event) => filterHeroes(event, props.dispatch)}>
+          <input placeholder='Hero Name' />
+        </Form.Field>
+      </Form>
+    </Container>
   )
 }
 
-export default HeroesSearch
+export default connect()(HeroesSearch)
