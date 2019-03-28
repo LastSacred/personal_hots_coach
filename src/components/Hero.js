3fb5e4ffd324e64fe9ef6140d27b99c2301f7b4b
Arthur Torres <arthurtorres002@gmail.com>
2019-03-27 21:02:53 -0400

    remove import of Section from Heroes

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Image, Button, Section } from 'semantic-ui-react'
+import { Image, Button } from 'semantic-ui-react'
 
 import emptyIcon from '../assets/empty.jpg'
 import { getHeroes } from '../services/api'
